50484bc0df4741c3835bc78c9a6290e49b90a08a
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-06-20 23:14:54 +0100

    added post to notes api, started delete method to api, added uuid

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require('express').Router();
-const 
-{ readFromFile, 
+const uuid = require('../helpers/uuid')
+const { readFromFile, 
     writeToFile, 
     readAndAppend 
 } = require('../helpers/fsUtils')
@@ -12,6 +12,35 @@ notes.get('/', (req, res) => {
     .then((data) => res.json(JSON.parse(data)))
     .catch((error) => error ? console.warn(error) : console.log('Connecting to ../api/notes'))
     console.log('GET for /api/notes')
-})
+});
+
+notes.post('/', (req, res) => {
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid,
+        }
+        console.log(newNote)
+        readAndAppend(newNote, './db/db.json');
+    } else {
+        res.error('There was an unexpected problem..')
+    }
+
+});
+
+notes.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    readAndAppend('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((resp) => {
+            console.log(resp)
+        });
+});
+
+console.log(uuid)
 
 module.exports = notes;
