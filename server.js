81a2701a34edf91531612c7f51f5fa766de5e696
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-06-20 22:22:40 +0100

    added fsUtils,js, api/notes route displays json

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 app.use('/api', api);
 
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('*', (req, res) => 
